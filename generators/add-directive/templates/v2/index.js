6d15adb56fb88ebb412fa9ee8251174c067a0f19
daniel.xiao <daniel.xiao@zydeiMac.local>
2017-01-23 16:59:41 +0800

    update v2 directive template

--- generators/add-directive/templates/v2/index.js
+++ generators/add-directive/templates/v2/index.js
@@ -3,17 +3,14 @@ import Vue from 'vue';
 import _assign from 'lodash-es/assign';
 
 Vue.directive('<%= directiveName %>', {
-  bind() {
-
-  },
-  update(options) {
+  bind(el, binding) {
     var defOptions = {
       msg: 'Hello', // TODO 修改成实际的参数名
     };
 
-    options = _assign(defOptions, options);
+    var options = _assign(defOptions, binding.value);
 
-    this.el.innerText = options.msg; // TODO 修改成实际的逻辑
+    el.innerText = options.msg; // TODO 修改成实际的逻辑
   },
   unbind() {
 
